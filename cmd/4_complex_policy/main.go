a8c558604db3a88d4464caec85ad14589b0b980e
Oleg Kozyrev <ovkozyrev@avito.ru>
2024-12-01 23:07:39 +0300

    small fix

--- cmd/4_complex_policy/main.go
+++ cmd/4_complex_policy/main.go
@@ -120,7 +120,7 @@ func checkAccess(ctx context.Context, testCase teatCase) (result, error) {
 		// Второй аргумент:
 		// Дополнительные опции загрузки, которые можно настроить, например,
 		// для включения и исключения определенных файлов. Можно передать nil, если нет особых требований.
-		rego.Load([]string{"./resource_check_policy.tmpl", "./permission_check_policy.tmpl", "./final_check_policy.tmpl"}, nil),
+		rego.Load([]string{"./resource_check.rego", "./permission_check.rego", "./final_check.rego"}, nil),
 	).
 		// Метод PrepareForEval используется для предварительной подготовки
 		// запроса, чтобы его можно было повторно использовать с разными входными
